f252e69235649371589a137ee53c41231e46ff52
Paulo Ricardo <pauloricardo.ggs@gmail.com>
2022-03-17 20:41:08 -0300

    Initial

---
+++ lfa_trabalhopratico1_20221/lfa_trabalhopratico1_20221/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lfa_trabalhopratico1_20221
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
